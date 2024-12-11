a468fddda0b73bc5945f2c3a371beae8d934e1b4
Aidan Hildebrand <blackbirdgt3rs@gmail.com>
2024-12-11 08:56:09 -0800

    Re Completes Lab using cleaner methods, cleans up code

--- index.js
+++ index.js
@@ -2,30 +2,28 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4"; 
 
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(leftNumbers, 10);
-
-    if (left > 0) {
-        dodger.style.left = `${left - 1}px`;
-    }
-}
-
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px","");
-    const left = parseInt(leftNumbers, 10);
-
-    if (left < 360) {
-        dodger.style.left = `${left + 1}px`;
-    }
-}
-
-
-document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-        moveDodgerLeft();
-    } else if (e.key === "ArrowRight") {
-        moveDodgerRight();
-    }
+//EventListener
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){ moveDodgerLeft()}
+    if (e.key === "ArrowRight"){moveDodgerRight()}
 })
 
+//Move left
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+//Move Right
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px","");
+    const right = parseInt(rightNumbers, 10)
+
+    if (right < 360){
+        dodger.style.left = `${right + 1}px`
+    }
+}
